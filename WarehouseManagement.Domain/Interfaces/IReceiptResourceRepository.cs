695dc4536336b101b442e52effb3a56eb451287f
Kirill Zhestkov <infinity.crime@outlook.com>
2025-08-02 19:48:57 +0300

    Добавлены интерфейсы для каждого репозитория, а также общий для всех интерфейсов.

---
+++ WarehouseManagement.Domain/Interfaces/IReceiptResourceRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseManagement.Domain.Entities;
+
+namespace WarehouseManagement.Domain.Interfaces
+{
+    public interface IReceiptResourceRepository : IRepository<ReceiptResource> { }
+}
