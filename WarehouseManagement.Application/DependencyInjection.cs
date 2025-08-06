bb4837a4fe4961b9c3482a3c18290ca4f810ed64
Kirill Zhestkov <infinity.crime@outlook.com>
2025-08-06 15:09:20 +0300

    Добавлено использование сервисов в методе расширения для добавления Application слоя.

--- WarehouseManagement.Application/DependencyInjection.cs
+++ WarehouseManagement.Application/DependencyInjection.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using WarehouseManagement.Application.Interfaces;
+using WarehouseManagement.Application.Services;
 
 namespace WarehouseManagement.Application
 {
@@ -13,6 +15,11 @@ namespace WarehouseManagement.Application
         public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddScoped<IResourceService, ResourceService>();
+            services.AddScoped<IUnitOfMeasureService, UnitOfMeasureService>();
+            services.AddScoped<IReceiptService, ReceiptService>();
+
             return services;
         }
     }
